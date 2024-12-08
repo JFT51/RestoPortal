95358579574667387ec14c34ffe28e00c089ec51
JFT51 <jeanfthibaut@gmail.com>
2024-12-08 14:24:11 +0100

    Register Chart.js components and plugins

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,30 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Register Chart.js components
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarController
+} from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarController,
+  ChartDataLabels
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
